44ea1c690b363bad9d4bca6ce2483e41ff034b50
haidv <haidv@atomi.com.vn>
2024-03-18 09:06:39 +0700

    change secure key

--- src/main/java/vn/com/atomi/loyalty/common/controller/AuthenticationController.java
+++ src/main/java/vn/com/atomi/loyalty/common/controller/AuthenticationController.java
@@ -8,12 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import vn.com.atomi.loyalty.base.constant.RequestConstant;
 import vn.com.atomi.loyalty.base.data.ResponseData;
 import vn.com.atomi.loyalty.base.data.ResponseUtils;
 import vn.com.atomi.loyalty.base.security.Authority;
+import vn.com.atomi.loyalty.base.security.UserOutput;
 import vn.com.atomi.loyalty.common.dto.input.LoginInput;
 import vn.com.atomi.loyalty.common.dto.output.LoginOutput;
-import vn.com.atomi.loyalty.base.security.UserOutput;
 import vn.com.atomi.loyalty.common.service.AuthenticationService;
 
 /**
@@ -58,7 +59,14 @@ public class AuthenticationController {
   @Operation(summary = "Api (nội bộ) lấy thông tin người dùng theo username")
   @PreAuthorize(Authority.ROLE_SYSTEM)
   @GetMapping("/internal/auth/user")
-  public ResponseEntity<ResponseData<UserOutput>> getUser(@RequestParam String username) {
+  public ResponseEntity<ResponseData<UserOutput>> getUser(
+      @Parameter(
+              description = "Chuỗi xác thực khi gọi api nội bộ",
+              example = "eb6b9f6fb84a45d9c9b2ac5b2c5bac4f36606b13abcb9e2de01fa4f066968cd0")
+          @RequestHeader(RequestConstant.SECURE_API_KEY)
+          @SuppressWarnings("unused")
+          String apiKey,
+      @RequestParam String username) {
     return ResponseUtils.success(authenticationService.getUser(username));
   }
 }
